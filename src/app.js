41c4949bacda5b38d7969b3b6c4c47af6542be99
Jeroen Ingelbrecht <jeroen.ingelbrecht@kbc.be>
2017-06-09 11:22:42 +0200

    add states

--- src/app.js
+++ src/app.js
@@ -1,7 +1,23 @@
 const Alexa = require('alexa-sdk');
 
 const MSG_ERROR = 'Oops, something went wrong.  You know what, let\'s call it a day.';
-const MSG_START = 'Ok, let\'s start, you tiger!!!  Go, go, hackathon!!';
+
+const MSG_START = 'Hello.  How can I help you?';
+
+const MSG_ROOM_ANSWER = 'The room for the training is Georges Minnen';
+
+const MSG_LOCATION_ANSWER = 'Yes, it\'s up the escalator and the first room on the left';
+
+const MSG_RESTAURANT_ANSWER = 'Sure, if you exit the building take the first street on the right.  On the corner you can find a nice Italian restaurant';
+
+const MSG_BENEFIT_ANSWER = 'Yes, you just have to show your badge';
+
+const MSG_RESTAURANT_THANK_YOU_ANSWER = 'Enjoy your meal';
+
+const MSG_THANK_YOU_ANSWER = 'You\'re welcome!  Have a nice day';
+
+
+
 
 const MSG_HELP = 'This is a help message to help you on your way.';
 
@@ -28,6 +44,11 @@ module.exports.starthandler = Alexa.CreateStateHandler(states.START, {
 	Start(){
 		this.emit(':ask', MSG_START, MSG_HELP);
 	},
+
+	'RoomIntent' : function(){
+		this.emit(':ask', MSG_ROOM_ANSWER);
+	},
+
 	Unhandled(){
 		this.handler.state = states.ERROR;
 		this.emitWithState('Unhandled');
