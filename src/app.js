cc258a5433ad69049c7f39c420cdae08235234be
Jeroen Ingelbrecht <jeroen.ingelbrecht@kbc.be>
2017-06-09 14:23:04 +0200

    add states

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const MSG_ERROR = 'Oops, something went wrong.  You know what, let\'s call it a 
 
 const MSG_START = 'Hello.  How can I help you?';
 
+// trainingroom
 const MSG_ROOM_ANSWER = 'The room for the training is Georges Minnen';
 
 const MSG_LOCATION_ANSWER = 'Yes, it\'s up the escalator and the first room on the left';
@@ -13,6 +14,22 @@ const MSG_RESTAURANT_ANSWER = 'Sure, if you exit the building take the first str
 const MSG_BENEFIT_ANSWER = 'Yes, you just have to show your badge';
 
 const MSG_RESTAURANT_THANK_YOU_ANSWER = 'Enjoy your meal';
+
+// Bus
+const MSG_TRAIN_BUS_ANSWER = 'Perhaps you can take a bus at half past four.  By the way, between four and five pm there is a bus every five minutes';
+
+
+// Holiday
+
+const MSG_HOLIDAY = 'Should I do the necessary for you';
+
+const MSG_YES = 'Your request is sent to your manager.  Can I do anything else for you?';
+
+const MSG_DAYS_LEAVE_LEFT = 'You still have twenty vacation days left';
+
+const MSG_NICE = 'Ok.  You have to hurry because the bus will be there in two minutes.  Bye!'; //tell
+
+const MSG_NO = 'Have a nice day';
 
 const MSG_THANK_YOU_ANSWER = 'You\'re welcome!  Have a nice day';
 
@@ -67,6 +84,36 @@ module.exports.starthandler = Alexa.CreateStateHandler(states.START, {
 
 	'RestaurantThankYouIntent' : function(){
 		this.emit(':tell', MSG_RESTAURANT_THANK_YOU_ANSWER);
+	},
+
+	'TrainBusIntent' : function(){
+		this.emit(':tell', MSG_TRAIN_BUS_ANSWER);
+	},
+
+	'AMAZON.SearchAction<object@WeatherForecast>' : function(){
+		this.emit('AMAZON.SearchAction<object@WeatherForecast>', () => {
+			this.emit(':ask', '');	
+		});
+	}, 
+
+	'HolidayIntent' : function(){
+		this.emit(':ask', MSG_HOLIDAY);
+	},
+
+	'AMAZON.YesIntent' : function(){
+		this.emit(':ask', MSG_YES;
+	},
+
+	'DaysLeaveLeftIntent' : function(){
+		this.emit(':ask', MSG_DAYS_LEAVE_LEFT);
+	},
+
+	'NiceIntent' : function(){
+		this.emit(':tell', MSG_NICE);
+	},
+
+	'AMAZON.NoIntent' : function(){
+		this.emit(':tell', MSG_NO);
 	},
 
 	Unhandled(){
